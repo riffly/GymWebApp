7b3cfd57e5fdf6b3d8fbb206ea439f6516bc3a1d
riffly <iamyusufrifat@gmail.com>
2023-03-14 19:15:15 +0600

    fixed

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -15,14 +15,14 @@ const Footer = () => {
         </div>
         <div className="mt-16 basis-1/4 md:mt-0">
           <h4 className="font-bold">Links</h4>
-          <p className="my-5">Massa orci senectus</p>
-          <p className="my-5">Et gravida id et etiam</p>
-          <p>Ullamcorper vivamus</p>
+          <p className="my-5">Facebook</p>
+          <p className="my-5">Linked.in</p>
+          <p>gitHub</p>
         </div>
         <div className="mt-16 basis-1/4 md:mt-0">
           <h4 className="font-bold">Contact Us</h4>
-          <p className="my-5">Tempus metus mattis risus volutpat egestas.</p>
-          <p>(333)425-6825</p>
+          <p className="my-5">Rifat Yusuf</p>
+          <p>(+880)01723779197</p>
         </div>
       </div>
     </footer>
